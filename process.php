d6469297de5c36be522eefc88de1c9dbe00512e5
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-23 21:12:46 +0530

    Added functionality to store data to the database using post method.

--- process.php
+++ process.php
@@ -1,5 +1,10 @@
 <?php 
 
+// Connecting to Database
+// host, username, pass, db name
+$conn = mysqli_connect('localhost', 'root', '', 'ajaxtest');
+
+
 echo 'Processing...';
 // Check for Get variables
 if( isset($_GET['name']) ){
@@ -8,7 +13,16 @@ if( isset($_GET['name']) ){
 
 // Check for POST variables
 if( isset($_POST['name']) ){
-    echo 'POST: Your name is ' . $_POST['name'];
+    // echo 'POST: Your name is ' . $_POST['name'];
+    
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $query = "INSERT INTO users(name) VALUES('$name')";
+
+    if(mysqli_query($conn, $query)){
+        echo 'User added to database.';
+    } else {
+        echo 'Error: ' . mysqli_error($conn);
+    }
 }
 
 ?>
