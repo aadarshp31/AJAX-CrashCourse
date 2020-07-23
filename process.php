940149dcf68a9eb23250d3ab0e6552217549f174
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-23 19:37:31 +0530

    Created POST forms AJAX request as well as general post request.

--- process.php
+++ process.php
@@ -6,4 +6,9 @@ if( isset($_GET['name']) ){
     echo 'GET: Your name is ' . $_GET['name'];
 }
 
+// Check for POST variables
+if( isset($_POST['name']) ){
+    echo 'POST: Your name is ' . $_POST['name'];
+}
+
 ?>
